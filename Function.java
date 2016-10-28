7b72118edffadfaad612bffc94e13223d86d4168
gcace075 <gcace075@uottawa.ca>
2016-10-28 11:37:30 -0400

    ...

---
+++ Function.java
@@ -0,0 +1,4 @@
+public interface Function
+{
+   public double calculateValue(double x, double y); //you override this method in each controller
+}
--- RungeKutta.java
+++ RungeKutta.java
@@ -1,1 +1,35 @@
 public class RungeKutta
+
+{ 
+  public static double integrate(double x_0, double x_f, double y_0, double delx, int maxIt, Function f)
+  {
+    
+        double [] y;
+        double [] x;
+        double x[0]=x_0;
+        double y[0]=y_0;
+        int i=0;
+          
+        //communicate with the Function object, asking it for the right-hand-side value of the ode, given the current
+        //values of x and y. This is accomplished through the Function object's calculateValue method
+        while(x<=x_f && i<maxIt)
+        {
+          double k1, k2, k3, k4;
+          
+          k1=f.calculateValue(x[i],y[i]);
+          k2=f.calculateValue(x[i]+(delx*/2), y[i]+(delx*k1/2));
+          k3=f.calculateValue(x[i]+(delx*/2), y[i]+(delx*k2/2));
+          k4=f.calculateValue(x[i]+delx,y[i]+(delx*k3));
+            
+          y[i]=y[i]+delx*(k1+(2*k2)+(2*k3)+k4)/6;
+          x[i]=x[i]+delx;
+          i++;
+          
+        }
+
+        if(i==maxIt) System.out.println("endpoint not reached within maximum specified initerations in RK4 integrate method");
+        
+        return y;
+    }//end of integrate method
+  
+}//end of RK4 class
