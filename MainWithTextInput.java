3ffbe44b2356c38afbba3b2ebee914c0c114e469
Emcossar <ecoss089@uottawa.ca>
2016-11-11 13:39:38 -0500

    je travaille quand meme sur file i/o
    no need for you guys to look at this yet

--- MainWithTextInput.java
+++ MainWithTextInput.java
@@ -18,8 +18,49 @@ public class MainWithTextInput
       System.exit(0);
     }//end catch
     
-    int n1=inputStream.nextInt();
-    int n2=inputStream.nextInt();
+    while(!inputStream.hasNextInt())
+    {
+      System.out.println("You did not enter an integer for system selection. Plz modify the text file and try again.");
+      return;
+    }
+    int systemSelection=inputStream.nextInt();
+    
+    if (systemSelection==1)
+    {
+      
+    }//add code for CSTR heating
+    else if(systemSelection==2)
+    {
+      
+    }//ad code for level system
+    else 
+    {
+      System.out.println("Please chose either 1 or 2 depending on the process you would like to simulate.");
+    }
+    
+    inputStream.nextLine();
+    while(!inputStream.hasNextInt())
+    {
+      System.out.println("You did not enter an integer for input signal. Plz modify the text file and try again.");
+      return;
+    }
+    int inputSignal=inputStream.nextInt();
+    inputStream.nextLine();
+    
+    if (inputSignal==1)
+    {
+      double stepChange=inputStream.nextDouble();
+
+    }
+    else if (inputSignal==2)
+    {
+      double
+      
+    }
+    
+    
+    int =inputStream.nextInt();
+    int =inputStream.nextInt();
     
     inputStream.nextLine();//move to next line
     
@@ -29,6 +70,14 @@ public class MainWithTextInput
     
     inputStream.close();
     
+    while(!inputStream.hasNextInt())
+    {
+      inputStream.next();
+      System.out.println("You did not enter an integer for system selection. plz modifie the text file and try again");
+      return;
+    }
+    int num=inputStream.nextInt();
+    System.out.println("You finally entered an integer value of: "+num);
     
   }//end main
   
