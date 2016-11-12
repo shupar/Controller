4231d5b8ec6568e7ed523c7656bcf457f3cadc63
Emcossar <ecoss089@uottawa.ca>
2016-11-12 09:52:00 -0500

    lil update

--- MainWithTextInput.java
+++ MainWithTextInput.java
@@ -18,9 +18,10 @@ public class MainWithTextInput
       System.exit(0);
     }//end catch
     
+    //Scanning for process to simulate
     while(!inputStream.hasNextInt())
     {
-      System.out.println("You did not enter an integer for system selection. Plz modify the text file and try again.");
+      System.out.println("You did not enter an integer that suited the options for system selection. Plz modify the text file and try again.");
       return;
     }
     int systemSelection=inputStream.nextInt();
@@ -32,41 +33,79 @@ public class MainWithTextInput
     else if(systemSelection==2)
     {
       
-    }//ad code for level system
+    }//add code for level system
     else 
     {
-      System.out.println("Please chose either 1 or 2 depending on the process you would like to simulate.");
+      System.out.println("Please enter either 1 or 2 into the text file depending on the process you would like to simulate.");
+      return;
     }
-    
     inputStream.nextLine();
+    
+    //scanning for type of input signal to impose on selected process
     while(!inputStream.hasNextInt())
     {
-      System.out.println("You did not enter an integer for input signal. Plz modify the text file and try again.");
+      System.out.println("You did not enter an integer that suited the options for input signal. Plz modify the text file and try again.");
       return;
     }
     int inputSignal=inputStream.nextInt();
     inputStream.nextLine();
+    //System.out.println(inputSignal);
     
+    //if user choses a step change, scan for step
+    int stepChange=0;
     if (inputSignal==1)
     {
-      double stepChange=inputStream.nextDouble();
-
+      while(!inputStream.hasNextInt())
+      {
+      System.out.println("You did not enter an rounded (integer) value for your desired step. Plz modify the text file and try again.");
+      return;
+      }
+      stepChange=inputStream.nextInt();
     }
-    else if (inputSignal==2)
+    
+    //if user selects disturbance, scan for dusturbance magnitude and time
+    int disturbanceMag=0;
+    int disturbanceStart=0;
+    int disturbanceEnd=0;
+ 
+    if (inputSignal==2)
     {
-      double
+      inputStream.nextLine();
       
+      while(!inputStream.hasNextInt())
+      {
+      System.out.println("You did not enter an rounded (integer) value for your disturbance magnitude. Plz modify the text file and try again.");
+      return;
+      }
+      disturbanceMag=inputStream.nextInt();
+      
+      while(!inputStream.hasNextInt())
+      {
+      System.out.println("You did not enter an rounded (integer) value for the start time of your disturbance. Plz modify the text file and try again. Note: Ensure your time is in seconds.");
+      return;
+      }
+      disturbanceStart=inputStream.nextInt();
+      
+      System.out.println(disturbanceMag);
+      
+      while(!inputStream.hasNextInt())
+      {
+      System.out.println("You did not enter an rounded (integer) value for the end time of your disturbance. Plz modify the text file and try again. Note: Ensure your time is in seconds.");
+      return;
+      }
+      disturbanceEnd=inputStream.nextInt();
     }
+    else
+    {
+      System.out.println("Please enter either 1 or 2 into the text file depending on the type of change you would like to invoke on your process.");
+      return;
+    }
+      
+      System.out.println(disturbanceMag+disturbanceStart+disturbanceEnd);
     
-    
-    int =inputStream.nextInt();
-    int =inputStream.nextInt();
-    
-    inputStream.nextLine();//move to next line
+   /* inputStream.nextLine();//move to next line
     
     String line=inputStream.nextLine();//read whole next line
-    
-    System.out.println(n1+", "+n2+", "+line);
     
     inputStream.close();
     
@@ -77,7 +116,7 @@ public class MainWithTextInput
       return;
     }
     int num=inputStream.nextInt();
-    System.out.println("You finally entered an integer value of: "+num);
+    System.out.println("You finally entered an integer value of: "+num);*/
     
   }//end main
   
