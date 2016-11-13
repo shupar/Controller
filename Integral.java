adeb814a1483bc40ba271d277f448276753aca3e
shupar <evanshuparski@gmail.com>
2016-11-13 14:41:35 -0500

    Added Simpsons rule to the integral calculator

--- Integral.java
+++ Integral.java
@@ -26,7 +26,39 @@ public class Integral extends Controller
   
   public double calculateSignal(double step, int index, double[] error)
   {
+//    //Simpsons 1/3 requires even number of points
+//    if(error.length % 2 == 0 && error.length != 0 && error.length > 2)
+//    {
+//      for (int i =0;i<index;i++)
+//      {
+//        if (i == 0)
+//          integratedError += error[i];
+//        else if (i % 2 == 0)
+//          integratedError += 4*error[i];
+//        else if (i % 2 != 0)
+//          integratedError += 2*error[i];
+//      } //end loop
+//      
+//    } //end if
+//    integratedError = integratedError*step/3.0
+    
+    //Simpsons 3/8 requires an odd number of points
+//    if(error.length % 2 =! 0 && error.length != 0 && error.length > 2)
+//    {
+//      for (int i =0;i<index;i++)
+//      {
+//        if (i == 0)
+//          integratedError += error[i];
+//        else if (i % 2 == 0)
+//          integratedError += 3*error[i];
+//        else if (i % 2 != 0)
+//          integratedError += 2*error[i];
+//      } //end loop
+//      
+//    } //end if
+//    integratedError = 3.0*integratedError*step/8.0
     double integratedError=index*step*error[index];
+    
     return integratedError*getKC()/this.tauI;
   }
 }
